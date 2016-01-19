a3751dab1ff4496e6c25f9909ff0e9538d33d816
Alvin Luu <alvluu@yahoo.com>
2016-01-19 15:12:05 -0500

    added about view

--- rango/urls.py
+++ rango/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, url
 from rango import views
+from rango import about
 
-urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'))
+urlpatterns = [
+# patterns('', url(r'^$', views.index, name='index')),
+url(r'^$',views.index, name='index'),
+url(r'^about/$',about.index, name='index'),
+]
